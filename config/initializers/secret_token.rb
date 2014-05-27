f91db56813454319c8786a252b5e3d9f4d56c37e
Arjun Venkataswamy <you@example.com>
2014-05-27 23:15:52 +0000

    set up places resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuffToDo::Application.config.secret_key_base = '399d6df0b190c0a4d93f0b7fc0b0c6764a86692608039dfa77d1d48e050cfd3acca70bbcf832e4027235591bf8c5a1dc2d729bf9dc0cdc2df40371e352290e97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StuffToDo::Application.config.session_store :cookie_store, key: '_stuff-to-do_session'
