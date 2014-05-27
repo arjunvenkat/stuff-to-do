0a9f8f84a64401b1c61b2255b7b151f140715ed3
Arjun Venkataswamy <you@example.com>
2014-05-27 23:42:53 +0000

    adds bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 StuffToDo::Application.routes.draw do
   resources :places
+  
+  root 'pages#home'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
