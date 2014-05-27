4f4a41b8551d18bb2a8e146d1738c0ee7742231b
Arjun Venkataswamy <you@example.com>
2014-05-27 23:32:58 +0000

    adds geocoder and a category column to places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527231345) do
+ActiveRecord::Schema.define(version: 20140527233051) do
 
   create_table "places", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140527231345) do
     t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
 end
