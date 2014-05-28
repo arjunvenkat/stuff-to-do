f12a3df0fd137acc9efc93d40d989f43c1b8b713
Arjun Venkataswamy <you@example.com>
2014-05-28 00:06:40 +0000

    adds basic search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    if params['search']
+      @places = Place.where(category: params['search']) 
+    else
+      @places = Place.all
+    end
+    
   end
 end
