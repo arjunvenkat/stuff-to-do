5835eaa5f204b4d1267b53f4d4665825e4439c51
Arjun Venkataswamy <you@example.com>
2014-05-28 00:17:37 +0000

    adds fuzzy search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
     if params['search']
-      @places = Place.where(category: params['search']) 
+#       @places = Place.where(category: params['search']) 
+      places_by_category = Place.where("category LIKE '%#{params['search']}%'")
+      places_by_address = Place.where("address LIKE '%#{params['search']}%'")
+  
+    @places = places_by_category + places_by_address
     else
       @places = Place.all
     end
-    
   end
 end
